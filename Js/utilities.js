055daec857e5a27d78b5dffc53d1b7c9a220d455
mhmahdi97 <mhmahdi97@gmail.com>
2023-02-18 23:19:39 +0600

    create functions for area calculations

--- Js/utilities.js
+++ Js/utilities.js
@@ -20,3 +20,33 @@ function setTextElementValueById(elementId, newValue) {
   const textElement = document.getElementById(elementId);
   textElement.innerText = newValue;
 }
+
+// fucntion for calculating area of triangl
+function triangleArea(b, h) {
+  return 0.5 * b * h;
+}
+
+// function for calculating rectangle area
+function rectangleArea(w, l) {
+  return w * l;
+}
+
+// function for calculating parallelogram area
+function parallelogramArea(b, h) {
+  return b * h;
+}
+
+// function for calculating rhombus area
+function rhombusArea(d1, d2) {
+  return 0.5 * d1 * d2;
+}
+
+// function for calculating pentagon area
+function pentagonArea(p, b) {
+  return 0.5 * p * b;
+}
+
+// function for calculating ellipse area
+function ellipseArea(a, b) {
+  return 3.1416 * a * b;
+}
